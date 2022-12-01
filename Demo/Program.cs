cd10433b0f3449093776250f8d4de342f494fadf
Guangyan Li <GLi@stmartin.edu>
2022-12-01 08:58:20 -0800

    a

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,17 +1,39 @@
-﻿public class Program
+﻿using System;
+
+namespace CSC180Quiz03
 {
-    static void Main(string[] args)
+    public class Program
     {
-        // car1 does not have any information.
-        var car1 = new Vehicle();
-        var car2 = new Vehicle("1FM0122021", "Ford", "Mercury", 0);
-        car2.Drive(100);
-        Console.Write("Car #1: ");
-        car1.Display();
-        Console.Write("Car #2: ");
-        car2.Display();
-        car2.Drive(90);
-        Console.Write("After driving car #2 90 miles\nCar #2: ");
-        car2.Display();
+        static void Main(string[] args)
+        {
+            // car1 does not have any information.
+            var car1 = new Vehicle();
+            var car2 = new Vehicle() { Vin = "1FM0122021", Make = "Ford", Model = "Mercury", Mileage = 0 };
+            car2.Drive(100);
+            Console.Write("Car #1: ");
+            car1.Display();
+            Console.Write("Car #2: ");
+            car2.Display();
+            car2.Drive(90);
+            Console.Write("After driving car #2 90 miles\nCar #2: ");
+            car2.Display();
+        }
+    }
+
+    public class Vehicle
+    {
+        public string Vin { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Mileage { get; set; }
+
+        public void Drive(int miles)
+        {
+            this.Mileage += miles;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Vin: {Vin}, make: {Make}, Model: {Model}, Mileage: {Mileage}");
+        }
     }
 }
