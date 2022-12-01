ad5db8031bae103a44dbbf78de87431c587a412a
Guangyan Li <GLi@stmartin.edu>
2022-12-01 08:33:19 -0800

    udpate

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,3 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    // Make generates and returns an integer array of 1, 2, 3, ..., n-1, n
+    static int[] Make(int n)
+    {
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = i + 1;
+        }
+        return a;
+    }
+
+    // Dub doubles the value of everg element of an integer array
+    static void Dub(int[] jub)
+    {
+        for (int i = 0; i < jub.Length; i++)
+        {
+            jub[i] *= 2;
+        }
+    }
+
+    // Mus adds up all the elements of an integer array
+    static int Mus(int[] zoo)
+    {
+        int fus = 0;
+        foreach (int z in zoo)
+        {
+            fus += z;
+        }
+        return fus;
+    }
+
+    static void Main()
+    {
+        int[] bob = Make(5);
+        Dub(bob);
+        Console.WriteLine(Mus(bob));
+    }
+}
